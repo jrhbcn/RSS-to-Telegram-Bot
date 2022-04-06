813e7ffe156fa7e35daf85140720cfcfa37414ab
jrhbcn <jrhbcn@gmail.com>
2022-04-06 08:43:13 +0200

    Update telegramRSSbot.py

--- telegramRSSbot.py
+++ telegramRSSbot.py
@@ -136,7 +136,7 @@ def cmd_help(update, context):
         "\n\nThe current chatId is: " + str(update.message.chat.id) +
         "\n\nThe stored chatId is: " + str(chatid) +
         "\n\nThe value of SHOWTEXT is: " + str(show_text) +
-        "\n\nIf you like the project, star it on [GitHUb](https://github.com/jrhbcn/RSS-to-Telegram-Bot)")
+        "\n\nIf you like the project, star it on [GitHUb](https://github.com/jrhbcn/RSS\-to\-Telegram\-Bot)")
 
 
 def rss_monitor(context):
